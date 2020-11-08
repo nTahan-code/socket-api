66a18cc1fb49f53dbf6f3a73f087d6d48bf8dcdc
nTahan-code <nct19999@gmail.com>
2020-11-08 15:45:14 -0600

    Update server-socket.py
    Made the server work... Hopefully

--- ver1/server/server-socket.py
+++ ver1/server/server-socket.py
@@ -31,21 +31,69 @@ def client_connect(connection, address):
     connected = True
     logged_in = False
     name = "null"
+    user_name = ""
     while connected:
         c_input = receive_Client_data(connection)
         #Outside if statements test for commands
-        if(final.split(0)=="login"):
+        if(c_input.split(0)=="login"):
             if(logged_in == False):
-                name=usr_login(user_name, password)
-                if(name == "Null"):
+                name=usr_login(c_input.split(1), c_input.split(2))
+                if(name == "null"):
                     print("Unable to login")
-                    
-
+                    connection.sendall("Error, unable to login. Please verify user login actually exists!".encode("utf8"))
+                else:
+                    logged_in=True
+                    connection.sendall("Successfully logged in as".encode("utf8"))
+                    user_name = c_input.split(1)
+        elif(c_input.split(0)=="send"):
+            if(logged_in== True):
+                string = user_name + " " + c_input.split(' ',1)[1] #Appends username and user msg
+                connection.sendall(string.encode("utf8"))
             else:
-                print("User: ", user_name, " is already logged in!")
+                connection.sendall("You must be logged in to send messages".encode("utf8"))
+        elif(c_input.split(0)="logout"):
+            if(logged_in== True):
+                connection.sendall("Logging out user".encode("utf8"))
+                connection.close()
+                connected = False
+            else:
+                connection.sendall("Error! User must be logged in before logging out".encode("utf8"))
+        elif(c_input.split(0)=="newuser"):
+            if(logged_in==False):
+                testvar = new_user(c_input.split(1), c_input.split(2)) #Calls the new user function, checks to see if user already exists
+                if(testvar=="null"):
+                    connection.sendall("Error! User account was unable to be created".encode("utf8"))
+                else:
+                    connection.sendall("User account successfully created!".encode("utf8"))
+                
 
+
+def new_user(user_name, password):
+    dictionary = get_dictionary()
+    if user_name in dictionary:
+        return "null"
+    else:
+        append_User(user_name, password)
+        return user_name
+
+def append_User(user_name, password):
+    try:
+        filePtr = open("users.txt","a")
+        filePtr.write(user_name + " " + password)
+        filePtr.close()
+    except Exception as error:
+        print("Error: ", error)
 
 def usr_login(user_name, password):
+    dictionary = get_dictionary()
+    if user_name in dictionary:
+        reference_password = dictionary[user_name]
+        if(reference_password == password):
+            return user_name
+        else:
+            return "null"
+            
+def get_dictionary():
     try:
         filePtr = open("users.txt","r")
         file_lines = filePtr.readlines()
@@ -55,14 +103,10 @@ def usr_login(user_name, password):
             dictionary[line.strip(0)] = line.strip(1)
         
         filePtr.close()
-        if user_name in dictionary:
-            reference_password = dictionary[user_name]
-            if(reference_password == password):
-                return user_name
-            else:
-                return "null"
-            
+    except Exception as error:
+        print("Error: ",error)
 
+    return dictionary
 
 
 def receive_Client_data(connection):
